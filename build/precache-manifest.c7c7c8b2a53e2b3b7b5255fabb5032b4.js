5722ab0552c937c3054b912f62ff145ad6ecccec
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2019-07-31 09:59:01 -0700

    2

---
+++ build/precache-manifest.c7c7c8b2a53e2b3b7b5255fabb5032b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f8f1d3b6981f848aef36fd2fabd3e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa866e43b7c6ce4e7b54",
+    "url": "/static/css/main.658636a6.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "aa866e43b7c6ce4e7b54",
+    "url": "/static/js/main.04722eb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
